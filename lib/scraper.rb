e51a2a762f91e7c2f0e51d199a2df0864f6bc0b2
Morgan Farrow <morgancfarrow@gmail.com>
2020-05-03 13:24:54 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -20,8 +20,7 @@ def make_courses
       course.title = post.css("h2").text
       course.schedule = post.css(".date").text
       course.description = post.css("p").text
-      
-      
+      end
     end
 
 
